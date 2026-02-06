5662b9bb9922e35cc1e3ad847fef9b4498843ff2
Micheal Adrian Gener <adriangener01@gmail.com>
2026-02-06 19:27:04 +0800

    added base controller for rendering pages

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,32 @@
+<?php
+
+class BaseController
+{
+    protected function render($view, $data = [])
+    {
+        extract($data);
+        $viewPath = __DIR__ . "/../../views/{$view}.php";
+        $header = __DIR__ . "/../../views/partials/header.php";
+        $footer = __DIR__ . "/../../views/partials/footer.php";
+
+        if (file_exists($viewPath)) {
+            require_once $header;
+            require_once $viewPath;
+            require_once $footer;
+        } else {
+            die("View not found: {$viewPath}");
+        }
+    }
+
+    /**
+     * Renders a clean view (like Login/Register) without sidebars
+     */
+    protected function renderAuth($view, $data = [])
+    {
+        extract($data);
+
+        require_once __DIR__ . '/../../views/partials/auth_header.php';
+        require_once __DIR__ . "/../../views/{$view}.php";
+        require_once __DIR__ . '/../../views/partials/auth_footer.php';
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,18 +1,22 @@
 <?php
+
 namespace App\Models;
 
 use App\Core\DbConfig;
 
-class User {
+class User
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $dbConfig = new DbConfig();
         $this->db = $dbConfig->getConnection();
     }
 
     // Find user by username (for login)
-    public function findByUsername($username) {
+    public function findByUsername($username)
+    {
         $stmt = $this->db->prepare("SELECT * FROM users WHERE username = ?");
         $stmt->bind_param("s", $username);
         $stmt->execute();
@@ -21,15 +25,17 @@ class User {
     }
 
     // Create new user (for registration)
-    public function create($data) {
+    public function create($data)
+    {
         $hashedPassword = password_hash($data['password'], PASSWORD_DEFAULT);
         $stmt = $this->db->prepare("INSERT INTO users (username, password, role, first_name, last_name, email) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssss", 
-            $data['username'], 
-            $hashedPassword, 
-            $data['role'], 
-            $data['first_name'], 
-            $data['last_name'], 
+        $stmt->bind_param(
+            "ssssss",
+            $data['username'],
+            $hashedPassword,
+            $data['role'],
+            $data['first_name'],
+            $data['last_name'],
             $data['email']
         );
         return $stmt->execute();
