f2e32403aa39d509b0319da6aaf9de4bf4f7c302
Micheal Adrian Gener <adriangener01@gmail.com>
2026-02-08 16:16:34 +0800

    added announcement controller placeholder

---
+++ app/Controllers/Announcement.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+class AnnouncementController extends BaseController
+{
+    public function index()
+    {
+        
+    }
+
+    public function show($id)
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function edit($id)
+    {
+        
+    }
+
+    public function update($id)
+    {
+        
+    }
+
+    public function destroy($id)
+    {
+        
+    }
+
+}
+
+?>
